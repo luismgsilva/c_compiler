a12ab75e5747cbbdb17f0a7496089f92cabdd538
Luis Silva <luismgsilva@outlook.com>
2024-07-06 22:21:15 +0100

    [main.c] Add new template logic.
    Signed-off-by: Luis Silva <luismgsilva@outlook.com>

--- main.c
+++ main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
+#include "compiler.h"
 
 int
 main (void)
 {
-  printf("Hello World!\n");
-  return 0;
+	int res = compile_file("./test.c", "./test", 0);
+	if (res == COMPILER_FILE_COMPILED_OK)
+		printf("Everthing looks good.\n");
+	else if (res == COMPILER_FAILED_WITH_ERRORS)
+		printf("Compilation failed.\n");
+	else
+		printf("Unknown result for compile file\n");
+	return 0;
 }
