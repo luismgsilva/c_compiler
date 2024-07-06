b5ceb07758148654bcaba8948a305434195be810
Luis Silva <luismgsilva@outlook.com>
2024-07-06 21:21:35 +0100

    [compiler.c] Add new compiler process.
    Signed-off-by: Luis Silva <luismgsilva@outlook.com>

--- compiler.c
+++ compiler.c
@@ -1,5 +1,16 @@
+#include "compiler.h"
 int
 compile_file (const char* file_name, const char* out_file_name, int flags)
 {
-	return 0;
+	struct compile_process* process = compile_process_create(file_name, out_file_name, flags);
+	if (!process)
+		return COMPILER_FAILED_WITH_ERRORS;
+
+	/* Preform lexical analysis */
+
+	/* Preform parsing */
+
+	/* Preform code generation */
+
+	return COMPILER_FILE_COMPILED_OK;
 }
--- compiler.h
+++ compiler.h
@@ -2,6 +2,13 @@
 #define C_COMPILER_H
 
 #include <stdio.h>
+
+enum
+{
+	COMPILER_FILE_COMPILED_OK,
+	COMPILER_FAILED_WITH_ERRORS,
+};
+
 struct compile_process
 {
 	/* Flags on how this file should be compiled. */
