f1137286eec71ecec83a648e93fea97011776075
Luis Silva <luismgsilva@outlook.com>
2024-07-06 21:02:05 +0100

    Add compiler.{c,h}
    Signed-off-by: Luis Silva <luismgsilva@outlook.com>

---
+++ compiler.c
@@ -0,0 +1,5 @@
+int
+compile_file (const char* file_name, const char* out_file_name, int flags)
+{
+	return 0;
+}
---
+++ compiler.h
@@ -0,0 +1,5 @@
+#ifndef C_COMPILER_H
+#define C_COMPILER_H
+int compile_file (const char* file_name, const char* out_file_name, int flags);
+
+#endif
