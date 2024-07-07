7cbca1aaa24550b746bce4024e6f1e636f70bd3f
Luis Silva <luismgsilva@outlook.com>
2024-07-07 11:52:03 +0100

    [lexer] Add logic to push each token to vector
    Signed-off-by: Luis Silva <luismgsilva@outlook.com>

--- lexer.c
+++ lexer.c
@@ -1,8 +1,30 @@
 #include "compiler.h"
+#include "helpers/vector.h"
+
+static struct lex_process *lex_process;
+
+struct token
+*read_next_token()
+{
+    struct token *token = NULL;
+
+    return token;
+}
 
 int
 lex (struct lex_process *process)
 {
-    return 0;
-}
+    process->current_expression_count = 0;
+    process->parentheses_buffer = NULL;
+    lex_process = process;
+    process->pos.filename = process->compiler->cfile.abs_path;
 
+    struct token *token = read_next_token();
+    while (token)
+    {
+        vector_push(process->token_vec, token);
+        token = read_next_token();
+    }
+
+    return LEXICAL_ANALYSIS_ALL_OK;
+}
