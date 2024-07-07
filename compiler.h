fe56d2f9218e34157ca3033318fbbc199443751a
Luis Silva <luismgsilva@outlook.com>
2024-07-07 10:07:40 +0100

    [compiler.h] Add the headers for ...
    compile_process_next_char
    compile_process_peek_char
    compile_process_push_char
    
    Signed-off-by: Luis Silva <luismgsilva@outlook.com>

--- compiler.h
+++ compiler.h
@@ -125,4 +125,8 @@ struct compile_process
 int compile_file (const char* file_name, const char* out_file_name, int flags);
 struct compile_process *compile_process_create (const char* file_name, const char* out_file_name, int flags);
 
+char compile_process_next_char (struct lex_process *lex_process);
+char compile_process_peek_char (struct lex_process *lex_process);
+void compile_process_push_char (struct lex_process *lex_process, char c);
+
 #endif
