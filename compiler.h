0610a506fcf4abc7ce9a0ca415a173013d61f32c
Luis Silva <luismgsilva@outlook.com>
2024-07-07 12:26:42 +0100

    [compiler.h] Add numeric case switch definition.
    Signed-off-by: Luis Silva <luismgsilva@outlook.com>

--- compiler.h
+++ compiler.h
@@ -16,6 +16,18 @@ struct pos
 	const char *filename;
 };
 
+#define NUMERIC_CASE	\
+	case '0':	\
+	case '1':	\
+	case '2':	\
+	case '3':	\
+	case '4':	\
+	case '5':	\
+	case '6':	\
+	case '7':	\
+	case '8':	\
+	case '9'
+
 enum
 {
 	LEXICAL_ANALYSIS_ALL_OK,
