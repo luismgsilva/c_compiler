85f770956e247301ba3243ab88ef499803acb4df
Luis Silva <luismgsilva@outlook.com>
2024-07-07 13:15:31 +0100

    [lexer.c] Add number tokenzier logic.
    Signed-off-by: Luis Silva <luismgsilva@outlook.com>

--- lexer.c
+++ lexer.c
@@ -1,5 +1,7 @@
 #include "compiler.h"
+#include <string.h>
 #include "helpers/vector.h"
+#include "helpers/buffer.h"
 
 #define LEX_GETC_IF(buffer, c, exp) 	\
     for (c =peekc(); exp; c = peekc(c))	\
@@ -9,6 +11,7 @@
     }
 
 static struct lex_process *lex_process;
+static struct token tmp_token;
 
 static char
 peekc()
@@ -34,6 +37,54 @@ pushc (char c)
     lex_process->function->push_char(lex_process, c);
 }
 
+static struct pos
+lex_file_position ()
+{
+    return lex_process->pos;
+}
+
+struct token
+*token_create (struct token *_token)
+{
+    memcpy(&tmp_token, _token, sizeof(struct token));
+    tmp_token.pos = lex_file_position();
+    return &tmp_token;
+}
+
+const char
+*read_number_str ()
+{
+    const char *num = NULL;
+    struct buffer *buffer = buffer_create();
+    char c = peekc();
+    /* Extract all the digits from 0 to 9 from a number.  */
+    LEX_GETC_IF(buffer, c, (c >= '0' && c <= '9'));
+
+    /* Write NULL terminator to the string (0xx0) */
+    buffer_write(buffer, 0x00);
+    return buffer_ptr(buffer);
+}
+
+unsigned long long
+read_number()
+{
+    const char *s = read_number_str();
+    /* Convert string to a long long integer and return it. */
+    return atoll(s);
+}
+
+struct token
+*token_make_number_for_value (unsigned long number)
+{
+    return token_create(&(struct token){.type=TOKEN_TYPE_NUMBER,.llnum=number});
+}
+
+struct token
+*token_make_number ()
+{
+    return token_make_number_for_value(read_number());
+}
+
 struct token
 *read_next_token()
 {
@@ -41,6 +92,9 @@ struct token
     char c = peekc();
     switch (c)
     {
+        NUMERIC_CASE:
+            token = token_make_number();
+            break;
         case EOF:
             /* Finished lexical analysis on the file. */
             break;
