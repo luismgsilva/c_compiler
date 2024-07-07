d157d02b03d073ce93789380be48b684a743e8cf
Luis Silva <luismgsilva@outlook.com>
2024-07-07 10:44:56 +0100

    [compiler.c] Add lex_process_functions
    Signed-off-by: Luis Silva <luismgsilva@outlook.com>

--- compiler.c
+++ compiler.c
@@ -1,4 +1,11 @@
 #include "compiler.h"
+
+struct lex_process_functions compiler_lex_functions = {
+	.next_char = compile_process_next_char,
+	.peek_char = compile_process_peek_char,
+	.push_char = compile_process_push_char
+};
+
 int
 compile_file (const char* file_name, const char* out_file_name, int flags)
 {
@@ -7,7 +14,7 @@ compile_file (const char* file_name, const char* out_file_name, int flags)
 		return COMPILER_FAILED_WITH_ERRORS;
 
 	/* Preform lexical analysis */
-
+	struct lex_process *lex_process = lex_process_create(process, &compiler_lex_functions, NULL);
 	/* Preform parsing */
 
 	/* Preform code generation */
