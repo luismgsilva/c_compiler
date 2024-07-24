b33e758c1334115563601ab243c44b5ee0891048
Luis Silva <luismgsilva@outlook.com>
2024-07-24 20:43:46 +0100

    [compiler.h] Add parser structures.
    Signed-off-by: Luis Silva <luismgsilva@outlook.com>

--- compiler.h
+++ compiler.h
@@ -188,6 +188,71 @@ struct compile_process
 	FILE *ofile;
 };
 
+enum
+{
+	NODE_TYPE_EXPRESSION,
+	NODE_TYPE_EXPRESSION_PARENTHESES,
+	NODE_TYPE_NUMBER,
+	NOE_TYPE_IDENTIFIER,
+	NODE_TYPE_STRING,
+	NODE_TYPE_VARIABLE,
+	NODE_TYPE_VARIABLE_LIST,
+	NODE_TYPE_FUNCTION,
+	NODE_TYPE_BODY,
+
+	NODE_TYPE_STATEMENT_RETURN,
+	NODE_TYPE_STATEMENT_IF,
+	NODE_TYPE_STATEMENT_ELSE,
+	NODE_TYPE_STATEMENT_WHILE,
+	NODE_TYPE_STATEMENT_DO_WHILE,
+	NODE_TYPE_STATEMENT_FOR,
+	NODE_TYPE_STATEMENT_BREAK,
+	NODE_TYPE_STATEMENT_CONTINUE,
+	NODE_TYPE_STATEMENT_SWITCH,
+	NODE_TYPE_STATEMENT_CASE,
+	NODE_TYPE_STATEMENT_DEFAULT,
+	NODE_TYPE_STATEMENT_GOTO,
+
+	NODE_TYPE_UNARY,
+	NODE_TYPE_TENARY,
+	NODE_TYPE_LABEL,
+	NODE_TYPE_STRUCT,
+	NODE_TYPE_UNION,
+	NODE_TYPE_BRACKET,
+	NODE_TYPE_CAST,
+
+	/*
+	 * A node that doesnt mean anything - like void.
+	 * And can be ignored.
+	 */
+	NODE_TYPE_BLANK
+};
+
+struct node
+{
+	int type;
+	int flags;
+
+	struct pos pos;
+
+	struct node_binded
+	{
+		/* Pointer to body node. */
+		struct node *owner;
+
+		/* Pointer to the function this node is in. */
+		struct node *function;
+	} binded;
+
+	union {
+		char cval;
+		const char *val;
+		unsigned int inum;
+		unsigned long lnum;
+		unsigned long long llnum;
+	};
+};
+
 int compile_file (const char* file_name, const char* out_file_name, int flags);
 struct compile_process *compile_process_create (const char* file_name, const char* out_file_name, int flags);
 
