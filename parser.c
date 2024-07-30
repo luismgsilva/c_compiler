345dd0dd663a536bcb680617d93e1ef5665f9154
Luis Silva <luismgsilva@outlook.com>
2024-07-30 21:08:09 +0100

    [parser.c] Remove extra space.
    Signed-off-by: Luis Silva <luismgsilva@outlook.com>

--- parser.c
+++ parser.c
@@ -166,7 +166,7 @@ parser_node_shift_children_left (struct node* node)
 void
 parser_reorder_expression (struct node** node_out)
 {
-    struct node * node = *node_out;
+    struct node* node = *node_out;
     if (node->type != NODE_TYPE_EXPRESSION)
     {
         return;
