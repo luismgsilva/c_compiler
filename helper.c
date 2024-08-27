31776b5c2c43d5091051fe0c822149b3d06b2ec0
Luis Silva <luismgsilva@outlook.com>
2024-08-27 20:43:39 +0100

    [helper.c] Implement alignment and padding functions.
    Signed-off-by: Luis Silva <luismgsilva@outlook.com>

--- helper.c
+++ helper.c
@@ -25,3 +25,43 @@ variable_size_for_list (struct node* var_list_node)
 
     return size;
 }
+
+int
+padding (int val, int to)
+{
+    if (to <= 0)
+    {
+        return 0;
+    }
+
+    /* 21 % 4 = 1*/
+    if ((val % to) == 0)
+    {
+        return 0;
+    }
+
+    /* 4 - (21 % 4) % 4 = 3 bytes padded to align 21 to 4 byte alignment. */
+    return to - (val % to) % to;
+}
+
+int
+align_value (int val, int to)
+{
+    if (val % to)
+    {
+        val += padding(val, to);
+    }
+
+    return val;
+}
+
+int align_value_treat_positive (int val, int to)
+{
+    assert(to >= 0);
+    if (val < 0)
+    {
+        to = -to;
+    }
+
+    return align_value(val, to);
+}
