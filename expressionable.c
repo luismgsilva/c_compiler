7fe25fce7eff3973c55a53e52d76fe71721db9dd
Luis Silva <luismgsilva@outlook.com>
2024-11-17 16:58:22 +0000

    parser: Fix expressionable equal sign.
    Signed-off-by: Luis Silva <luismgsilva@outlook.com>

--- expressionable.c
+++ expressionable.c
@@ -22,6 +22,6 @@ struct expressionable_op_precedence_group op_precedence[TOTAL_OPERATOR_GROUPS] =
     {.operators={"&&", NULL}, .associtivity=ASSOCIATIVITY_LEFT_TO_RIGHT},
     {.operators={"||", NULL}, .associtivity=ASSOCIATIVITY_LEFT_TO_RIGHT},
     {.operators={"?", ":", NULL}, .associtivity=ASSOCIATIVITY_RIGHT_TO_LEFT},
-    {.operators={"+", "+=", "-=", "*=", "/=", "%=", "<<=", ">>=", "&=", "^=", "|=", NULL}, .associtivity=ASSOCIATIVITY_RIGHT_TO_LEFT},
+    {.operators={"=", "+=", "-=", "*=", "/=", "%=", "<<=", ">>=", "&=", "^=", "|=", NULL}, .associtivity=ASSOCIATIVITY_RIGHT_TO_LEFT},
     {.operators={",", NULL}, .associtivity=ASSOCIATIVITY_LEFT_TO_RIGHT}
 };
