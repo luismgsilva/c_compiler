1938589e1e694d4bc35e79051970a63f59fc3dfc
Luis Silva <luismgsilva@outlook.com>
2024-11-19 22:38:58 +0000

    codegen: Add iteration of the AST.
    Signed-off-by: Luis Silva <luismgsilva@outlook.com>

--- codegen.c
+++ codegen.c
@@ -1,8 +1,27 @@
 #include "compiler.h"
 #include <stdarg.h>
 #include <stdio.h>
+#include "helpers/vector.h"
 
 static struct compile_process* current_process = NULL;
+
+void
+codegen_new_scope (int flags)
+{
+    #warning "The resolver needs to exist for this to work."
+}
+
+void
+codegen_finish_scope ()
+{
+    #warning "The resolver needs to exist for this to work."
+}
+
+struct node*
+codegen_node_next ()
+{
+    return vector_peek_ptr (current_process->node_tree_vec);
+}
 
 void
 asm_push_args (const char* insn, va_list args)
@@ -27,10 +46,67 @@ asm_push (const char* insn, ...)
     va_end (args);
 }
 
+void
+codegen_generate_data_section_part (struct node* node)
+{
+    /* Switch to process the global data..  */
+
+}
+
+void
+codegen_generate_data_section ()
+{
+    asm_push ("section .data");
+    struct node* node = codegen_node_next ();
+    while ((node = codegen_node_next ()) != NULL)
+    {
+        codegen_generate_data_section_part (node);
+    }
+}
+
+void
+codegen_generate_root_node (struct node* node)
+{
+    /* Process any functions.  */
+}
+
+void
+codegen_generate_root ()
+{
+    asm_push ("section .text");
+    struct node* node = NULL;
+    while ((node = codegen_node_next ()) != NULL)
+    {
+        codegen_generate_root_node (node);
+    }
+}
+
+void
+codegen_write_strings ()
+{
+    #warning "Loop through the string table and write all the strings"
+}
+
+void
+codegen_generate_readonly ()
+{
+    asm_push ("section .rodata");
+    codegen_write_strings ();
+}
+
 int
 codegen (struct compile_process* process)
 {
     current_process = process;
-    asm_push ("jmp %s", "label_name");
+    scope_create_root (process);
+    vector_set_peek_pointer (process->node_tree_vec, 0);
+    codegen_new_scope (0);
+    codegen_generate_data_section ();
+    vector_set_peek_pointer (process->node_tree_vec, 0);
+    codegen_generate_root ();
+    codegen_finish_scope ();
+
+    /* Generate read only data.  */
+    codegen_generate_readonly ();
     return 0;
 }
